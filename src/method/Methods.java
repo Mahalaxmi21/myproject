8482bbdd0ca18592b892491bd524e70ce6e8b18c
Mahalaxmi21 <mahalaxmihavalad21@gmail.com>
2022-02-02 10:51:53 +0530

    Method

---
+++ src/method/Methods.java
@@ -0,0 +1,16 @@
+package method;
+
+public class Methods {
+	void pen() {
+		System.out.println("pen is blue");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Method program");
+		Methods p = new Methods();
+		p.pen();//calling method
+		
+
+	}
+
+}
